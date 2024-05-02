58aed7ba6bc734044e98e367cd7e6b59f2dd641a
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-03 00:43:02 +0200

    Finally got the app working

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 struct SnakeHead;
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn(Camera2dBundle::default());
 }
 
 fn spawn_snake(mut commands: Commands) {
@@ -31,8 +31,7 @@ fn spawn_snake(mut commands: Commands) {
 
 fn main() {
     App::new()
-        .add_startup_system(setup_camera)
-        .add_startup_system(spawn_snake)
+        .add_systems(Update,(setup_camera, spawn_snake))
         .add_plugins(DefaultPlugins)
         .run();
 }
