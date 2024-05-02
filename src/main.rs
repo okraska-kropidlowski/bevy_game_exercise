a5118c2b8c29b53e5becc660165bfc1e6e7284bc
okraska-kropidlowski <wojtek.1986@gmail.com>
2024-05-03 00:08:43 +0200

    Start after updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
